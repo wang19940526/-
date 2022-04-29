868cd68558410d8d0a0381ac0f2537c9fb7c2cdf
wang19940526 <40156308+wang19940526@users.noreply.github.com>
2022-04-29 14:41:21 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/往下点/03、🏃🏻‍♂️撩妹会员课程', '/往下点/04、💃撩汉会员课程', '/往下点/05、🈲杏福课会员'],
+  protectedRoutes: ['/03、🏃🏻‍♂️撩妹会员课程', '/04、💃撩汉会员课程', '/05、🈲杏福课会员'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:spencer.wushangbo@gmail.com',
